79b4741aed4c467c0f1a98ae45df3c7e8a2d3451
Clear008 <wissal-exo@hotmail.com>
2026-07-03 13:24:41 +0100

    fix(documents): fix documents TAb of Clientdetails

--- src/pages/clients/ClientDetails.jsx
+++ src/pages/clients/ClientDetails.jsx
@@ -256,8 +256,9 @@ export default function ClientDetails() {
                     )}
                 </div>
             )}
+
             {/* Tab: Documents */}
-            {activeTab === 'Documents' && (
+            {activeTab === 'documents' && (
                 <div className="bg-gray-800 border border-gray-700 rounded-xl p-6">
                     <DocumentsTab entityType="CLIENT" entityId={client.id} />
                 </div>
